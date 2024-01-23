733a353fb543c18b8e6ce8e463cbf3581b0aeb03
Wintoch <kub123-2001@wp.pl>
2024-01-23 16:56:23 +0100

    se mapke dodalem bo mialem 5 minut

--- screens/Tabs/Map.jsx
+++ screens/Tabs/Map.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import MapView from 'react-native-maps'
 
 function Map() {
   return (
     <SafeAreaView>
-      <Text>Map</Text>
+      <MapView
+        style={styles.map}
+      />
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  map:{
+    height:'100%',
+    width: '100%',
+  }
+})
 
 export default Map
