dde7d684858ed7e62af53eb1b1737123078d012f
Charon6000 <hubertwit2005@gmail.com>
2024-01-30 20:04:11 +0100

    Dodanie pineski Gdańska

--- screens/Tabs/Map.jsx
+++ screens/Tabs/Map.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { SafeAreaView, Text, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps'
+import MapViewDirections from 'react-native-maps-directions';
 
 function Map() {
   return (
     <SafeAreaView>
       <MapView
+      showsUserLocation={true}
         style={styles.map}
-      />
+        provider={PROVIDER_GOOGLE}
+        region={{
+            latitude: 54.372158,
+            longitude: 18.638306,
+            latitudeDelta: 0.3,
+            longitudeDelta: 0.2,
+          }}>
+          <Marker coordinate={{ latitude: 54.372158, longitude: 18.638306 }}>
+            <Callout>
+              <Text>Gdańsk</Text>
+            </Callout>
+          </Marker>
+          <MapViewDirections
+            origin={{ latitude: 54.372158, longitude: 18.638306 }}
+            destination={{ latitude: 40.372158, longitude: 10.638306 }}
+          />
+        </MapView>
     </SafeAreaView>
   )
 }
